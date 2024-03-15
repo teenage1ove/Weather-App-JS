bdb3d723d144b41e5ec85c193a887258366fd57c
teenage1ove <kirillstars7@gmail.com>
2024-03-15 10:30:55 +0300

    Add function for IP search

--- scripts/script.js
+++ scripts/script.js
@@ -2,54 +2,71 @@ import variables from "./variables.js";
 
 const {search, container, input, error404, weatherBox, weatherDetails, image, temperature, humidity, description, wind} = variables
 
+search.addEventListener('click', getWeather)
 
-search.addEventListener('click', () => {
+
+function getPosition() {
+    navigator.geolocation.getCurrentPosition((position) => {
+        const { latitude, longitude } = position.coords
+        return fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude.toFixed(2)}&lon=${longitude.toFixed(2)}&lang=ru&exclude=current&units=metric&appid=f2cc519d51fe5cd6022f0c3916b980f7`)
+        .then(response => response.json())
+        .then(json => {
+            console.log(json)
+            input.value = `${json.name}`
+            innerWeather(json)
+        })
+    })  
+}
+getPosition()
+
+function getWeather() {
     const city = input.value
     if (city === '') return
     
-    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&lang=ru&appid=f2cc519d51fe5cd6022f0c3916b980f7`).then((response) => response.json()).then(json => {
-        
-        if (json.cod === "404") {
-            container.style.height = '400px'
-            weatherBox.style.display = 'none'
-            weatherDetails.style.display = 'none'
-            error404.style.display = 'block'
-            error404.classList.add('fade-in')
-            return
-        }
-        error404.style.display = 'none'
-        error404.classList.remove('fade-in')
+    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&lang=ru&appid=f2cc519d51fe5cd6022f0c3916b980f7`).then((response) => response.json()).then(json => innerWeather(json))
+}
 
-        switch(json.weather[0].main) {
-            case 'Clouds': image.src = './images/cloud.png'
-                break;
+function innerWeather(json) {
+    if (json.cod === "404") {
+        container.style.height = '400px'
+        weatherBox.style.display = 'none'
+        weatherDetails.style.display = 'none'
+        error404.style.display = 'block'
+        error404.classList.add('fade-in')
+        return
+    }
+    error404.style.display = 'none'
+    error404.classList.remove('fade-in')
 
-            case 'Clear': image.src = './images/clear.png'
-                break;
+    switch(json.weather[0].main) {
+        case 'Clouds': image.src = './images/cloud.png'
+            break;
 
-            case 'Snow': image.src = './images/snow.png'
-                break;
+        case 'Clear': image.src = './images/clear.png'
+            break;
 
-            case 'Rain': image.src = './images/rain.png'
-                break;
+        case 'Snow': image.src = './images/snow.png'
+            break;
 
-            case 'Haze': image.src = './images/haze.svg'
-                break;
-        }
+        case 'Rain': image.src = './images/rain.png'
+            break;
 
-        temperature.innerHTML = `${parseInt(json.main.temp)}<span>°C</span>`
+        case 'Haze': image.src = './images/haze.svg'
+            break;
+    }
 
-        humidity.innerText = `${json.main.humidity}%`
+    temperature.innerHTML = `${parseInt(json.main.temp)}<span>°C</span>`
 
-        description.innerHTML = `${json.weather[0].description}`
-        console.log(wind)
-        wind.innerHTML = `${parseInt(json.wind.speed)}Km/h`
+    humidity.innerText = `${json.main.humidity}%`
 
-        weatherBox.style.display = 'block'
-        weatherDetails.style.display = 'flex'
+    description.innerHTML = `${json.weather[0].description}`
+    console.log(wind)
+    wind.innerHTML = `${parseInt(json.wind.speed)}Km/h`
 
-        weatherBox.classList.add('fade-in')
-        weatherDetails.classList.add('fade-in')
-        container.style.height = '590px'
-    })
-})
+    weatherBox.style.display = 'block'
+    weatherDetails.style.display = 'flex'
+
+    weatherBox.classList.add('fade-in')
+    weatherDetails.classList.add('fade-in')
+    container.style.height = '590px'
+}
